46bf52a2498f08baa1eb6e072a2a920839d27188
spacesparky <124715925+spacesparky@users.noreply.github.com>
2023-09-15 02:08:38 +0300

    lab 1 ready

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,9 +1,9 @@
 public class Palindrom {
         public static void main(String[] args) {
 
-            System.out.println(isPalindrome("Hello", reverseString("Hello")));
-            System.out.println(isPalindrome("hello", reverseString("hello")));
-            System.out.println(isPalindrome("hello", "world"));
+            System.out.println(isPalindrome("olo"));
+            System.out.println(isPalindrome("aboba"));
+            System.out.println(isPalindrome("hello"));
         }
         public static String reverseString(String str){
             StringBuilder ans = new StringBuilder();
@@ -14,18 +14,8 @@ public class Palindrom {
             return ans.toString();
         }
 
-        public static boolean isPalindrome(String str1, String str2) {
-            if (str1.length() != str2.length()) {
-                return false;
-            }
-
-            char[] charArray1 = str1.toCharArray();
-            char[] charArray2 = str2.toCharArray();
-            for (int i = 0; i<str1.length(); i++){
-                if (charArray1[i] != charArray2[charArray2.length - i-1]){
-                    return false;
-                }
-            }
-            return true;
+        public static boolean isPalindrome(String str1){
+            String str2 = reverseString(str1);
+            return str1.equals(str2);
         }
 }
